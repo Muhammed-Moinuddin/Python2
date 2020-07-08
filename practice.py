36e6adf86c1b9e8aad2007afbd57b941d74c6482
Muhammad Moinuddin <Muhammad Moinuddin>
2020-07-08 20:35:18 +0500

    Create practice.py
    While loop practice

---
+++ practice.py
@@ -0,0 +1,19 @@
+#print numbers 1 to n using while loop.
+n = int(input("Please enter a number: "))
+i = 1
+while i < n+1:
+ print(i)
+ i += 1
+#print first n even and odd numbers
+#for even
+n = int(input("Please enter a number: "))
+i = 2
+while i < n+1:
+  print(i)
+  i += 2
+#for odd
+n = int(input("Please enter a number: "))
+i = 1
+while i < n+1:
+  print(i)
+  i += 2
